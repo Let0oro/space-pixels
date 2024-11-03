08719c49bf7887bebee2e9e9a12424655c9fb258
Let0oro <oniiichan354@gmail.com>
2024-11-03 16:32:50 +0100

    update: modify fetch to call nginx inverse proxy for secure backend and permanency of cookies

--- src/utils/FrontFetch.ts
+++ src/utils/FrontFetch.ts
@@ -7,7 +7,7 @@ import {
 } from "../interfaces/FrontfetchInterface";
 
 export class FrontFetch {
-  private static baseUrl = "http://localhost:3000/api/";
+  private static baseUrl = "http://localhost:3001/api/";
 
   public static parseMethod: parseMethodInterface = {
     player: {
